3d2d5fb321e107000a88edf3d302eddf7871f0c3
phatnomenal <nguyenhongphat0210@gmail.com>
2025-11-26 09:36:12 +0700

    add new module

---
+++ src/config/config.module.ts
@@ -0,0 +1,11 @@
+import { Module, Global } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppConfigService } from './config.service';
+
+@Global()
+@Module({
+  imports: [ConfigModule],
+  providers: [AppConfigService],
+  exports: [AppConfigService],
+})
+export class AppConfigModule {}
